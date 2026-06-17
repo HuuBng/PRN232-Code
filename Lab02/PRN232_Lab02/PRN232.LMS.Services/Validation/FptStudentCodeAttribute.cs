126b9052e9d18bebe1717dfbca83b8921a695a74
HuuBng <89332259+HuuBng@users.noreply.github.com>
2026-06-17 20:49:40 +0700

    correct the code validation

--- Lab02/PRN232_Lab02/PRN232.LMS.Services/Validation/FptStudentCodeAttribute.cs
+++ Lab02/PRN232_Lab02/PRN232.LMS.Services/Validation/FptStudentCodeAttribute.cs
@@ -4,7 +4,7 @@ namespace PRN232.LMS.Services.Validation
 {
     public class FptStudentCodeAttribute : RegularExpressionAttribute
     {
-        public FptStudentCodeAttribute() : base("^[A-Z]{2}\\d{5}$")
+        public FptStudentCodeAttribute() : base("^[A-Z]{2}\\d{6}$")
         {
             ErrorMessage = "Student code must match FPTU format, for example SE19886 or CE18793.";
         }
